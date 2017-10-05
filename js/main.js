f76b45dc14c3a0e2112e1ecbd83b2dc9d9f3b385
Rinat A <rinatkins@gmail.com>
2017-10-05 12:35:26 +0300

    h2

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 window.onload = function(){
   //выравниваем высоту главной страницы под высоту клиента
   var height;
-  (document.body.clientHeight < 750) ? height = "900px" : height = document.body.clientHeight + "px";
+  (document.body.clientHeight < 900) ? height = "900px" : height = document.body.clientHeight + "px";
   var mainContainer = document.getElementsByClassName('main-container')[0];
   mainContainer.style.height = height;
-  console.log(document.body.clientHeight);
+  console.log(document.body.clientHeight, height);
 }
